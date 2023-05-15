c996566a0748944d7f42819bdbd1b0e67df257e1
aardvarkpepper <aardvarkpepper@gmail.com>
2023-05-14 23:14:12 -0400

    Add background transitions and images.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,18 +19,21 @@ import Product from "./components/Products/Product.jsx";
 import RetailerProducts from "./components/Retailers/RetailerProducts.jsx";
 import RetailerNewProduct from "./components/Retailers/RetailerNewProduct.jsx";
 import RetailerEditProduct from "./components/Retailers/RetailerEditProduct.jsx";
-import RetailerShowProduct from "./components/Retailers/RetailerShowProduct.jsx"
+import RetailerShowProduct from "./components/Retailers/RetailerShowProduct.jsx";
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 
 function App() {
   const [loggedInAs, setLoggedInAs] = useState({
     id: 1,
     first_name: "Guest",
     last_name: "User",
-    email:  "guestuser@nomail.com",
-    phone:  "(000) 000-0000",
+    email: "guestuser@nomail.com",
+    phone: "(000) 000-0000",
     address_street: "123 Elf Road",
     address_street2: null,
-    address_city:  "North Pole",
+    address_city: "North Pole",
     address_state: "Sea Ice",
     address_postal_code: "88888",
     payment_info: "Payment: Coal"
@@ -51,10 +54,12 @@ function App() {
   const [cart, setCart] = useState({});
 
   return (
-    <div className="App">
+    <div className="App background-slider">
       <Router>
         <header>
+
           <NavBar loggedInAs={loggedInAs} />
+
         </header>
         <main>
           <Routes>
@@ -62,17 +67,17 @@ function App() {
 
             <Route exact path="/customers" element={<Customers setLoggedInAs={setLoggedInAs} />} />
             <Route exact path="/customers/new" element={<NewCustomer />} />
-            <Route exact path="/customers/:id" element={<CustomerDetails setLoggedInAs={setLoggedInAs}/>} />
+            <Route exact path="/customers/:id" element={<CustomerDetails setLoggedInAs={setLoggedInAs} />} />
             <Route exact path="/customers/:id/edit" element={<EditCustomer setLoggedInAs={setLoggedInAs} />} />
             <Route exact path="/customers/:id/cart" element={<CustomerCart loggedInAs={loggedInAs} customerCart={cart[`customer${loggedInAs.id}`]} cart={cart} setCart={setCart} />} />
             <Route exact path="/customers/:id/history" element={<CustomerHistory loggedInAs={loggedInAs} />} />
 
-            <Route path="/products" element={<Products cart={cart} setCart={setCart} loggedInAs={loggedInAs}/>} />
+            <Route path="/products" element={<Products cart={cart} setCart={setCart} loggedInAs={loggedInAs} />} />
             <Route path="/products/:id" element={<Product />} />
-            
+
             <Route path="/retailer/products" element={<RetailerProducts />} />
             <Route path="/retailer/products/new" element={<RetailerNewProduct />} />
-            <Route exact path="/retailer/products/:id" element ={<RetailerShowProduct />} />
+            <Route exact path="/retailer/products/:id" element={<RetailerShowProduct />} />
             <Route exact path="/retailer/products/:id/edit" element={<RetailerEditProduct />} />
 
             <Route path="/*" element={<Error />} />
