9b1a9d54ff8330e840efe639c3250939292c2978
aardvarkpepper <aardvarkpepper@gmail.com>
2023-05-15 15:10:11 -0400

    Add sort function(s) to retailer products with css mouseover styling

--- frontend/src/components/Retailers/RetailerProducts.jsx
+++ frontend/src/components/Retailers/RetailerProducts.jsx
@@ -7,15 +7,17 @@ const API = process.env.REACT_APP_API_URL;
 
 export default function RetailerProducts() {
   const [retailerProducts, setRetailerProducts] = useState([]);
+  const [defaultSearch, setDefaultSearch] = useState("id");
 
   //sort function satisfies "front end calculation . . . displayed to user".
   useEffect(() => {
     axios.get(`${API}/products`)
       .then((response) => {
+        console.log("search by", defaultSearch)
         setRetailerProducts(response.data.sort((product1, product2) => {
-          if (product1.id < product2.id) {
+          if (product1[defaultSearch] < product2[defaultSearch]) {
             return -1;
-          } if (product1 > product2.id) {
+          } if (product1[defaultSearch] > product2.id[defaultSearch]) {
             return 1;
           } else {
             return 0;
@@ -23,19 +25,26 @@ export default function RetailerProducts() {
         }))
       })
       .catch((e) => console.warn("catch", e));
-  }, [])
+  }, [defaultSearch])
+
+  const sortme = (sortstring) => {
+    setDefaultSearch(sortstring);
+  };
+
+  console.log("render");
+
   return (
     <div>
       <Table striped border-dark responsive className="text-overlay">
         <thead>
           <tr>
-            <th>Product</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Qty</th>
-            <th>Card ID</th>
-            <th>Rarity</th>
-            <th>UPC</th>
+            <th>Product <span onClick={() => sortme("name")} className="mouseoverpointer">&#x2195;</span></th>
+            <th>Description <span onClick={() => sortme("description")}className="mouseoverpointer">&#x2195;</span></th>
+            <th>Price <span onClick={() => sortme("price")}className="mouseoverpointer">&#x2195;</span></th>
+            <th>Qty <span onClick={() => sortme("quantity_in_stock")}className="mouseoverpointer">&#x2195;</span></th>
+            <th>Card ID <span onClick={() => sortme("card_id")}className="mouseoverpointer">&#x2195;</span></th>
+            <th>Rarity <span onClick={() => sortme("card_rarity")}className="mouseoverpointer">&#x2195;</span></th>
+            <th>UPC <span onClick={() => sortme("product_upc")}className="mouseoverpointer">&#x2195;</span></th>
             <th>Go To Item</th>
           </tr>
         </thead>
